3d606b2aaaf1685e9a9a348d14323bf77e0cd01b
fiveham <fiveham@users.noreply.github.com>
2016-06-17 01:34:30 -0400

    add variations of Wrap.wrap and documentation

--- src/common/graph/Wrap.java
+++ src/common/graph/Wrap.java
@@ -69,10 +69,12 @@ public class Wrap<W> implements WrapVertex<W,Wrap<W>>{
 	 * gives each resulting Wrap neighbors from among the resulting Wraps. 
 	 * Two resulting Wraps are neighbors if {@code edgeDetector} 
 	 * {@link BiPredicate#test(Object,Object) says so}.</p>
-	 * @param unwrapped the collection of non-{@link common.graph.Vertex Vertex} nodes 
-	 * to be wrapped
-	 * @param edgeDetector outputs true if two elements of {@code unwrapped} are linked 
-	 * by an edge, false otherwise
+	 * <p>This method connects the nodes in {@code O(n^2)} time where 
+	 * {@code n = unwrapped.size()} by testing all possible pairs of nodes.</p>
+	 * <p>The generated edges are bidirectional.</p>
+	 * @param unwrapped raw nodes
+	 * @param edgeDetector outputs true if two raw nodes from {@code unwrapped} share 
+	 * an edge, false otherwise
 	 * @return a list of Wraps complete with populated neighbor lists
 	 */
 	public static <W> List<Wrap<W>> wrap(
@@ -84,13 +86,18 @@ public class Wrap<W> implements WrapVertex<W,Wrap<W>>{
 	public static final int VERTICES_PER_EDGE = 2;
 	
 	/**
+	 * <p>A utility method that wraps each element of {@code unwrapped} in a {@code T} 
+	 * specified by {@code wrapper} and gives each resulting {@code T} neighbors from 
+	 * among the resulting {@code T}s. Two resulting {@code T}s are neighbors if 
+	 * {@code edgeDetector} {@link BiPredicate#test(Object,Object) says so}.</p>
+	 * <p>This method connects the nodes in {@code O(n^2)} time where 
+	 * {@code n = unwrapped.size()} by testing all possible pairs of nodes.</p>
+	 * <p>The generated edges are bidirectional.</p>
 	 * 
-	 * @param unwrapped a collection of objects to be wrapped and connected
-	 * @param edgeDetector a BiPredicate that tests any and every pair 
-	 * of elements of {@code unwrapped} (after they've been wrapped by {@code wrapper}) 
-	 * to see whether they're adjacent, and if so, they are connected
-	 * @param wrapper a Function that accepts an element of {@code unwrapped} 
-	 * and wraps it with some type of WrapVertex before outputting it
+	 * @param unwrapped raw nodes
+	 * @param edgeDetector outputs true if and only if two raw nodes from 
+	 * {@code unwrapped} share an edge, false otherwise
+	 * @param wrapper wraps an element of {@code unwrapped} with some type of WrapVertex
 	 * @return a List of the elements of {@code unwrapped} wrapped via {@code wrapper} 
 	 * and connected according to {@code edgeDetector}
 	 */
@@ -103,6 +110,15 @@ public class Wrap<W> implements WrapVertex<W,Wrap<W>>{
 		return result;
 	}
 	
+	/**
+	 * <p>A utility method that connects the nodes in {@code wrapped} by brute force, 
+	 * trying every pair of them and testing the pair using {@code edgeDetector}.</p>
+	 * <p>This method operates in {@code O(n^2)} time, where 
+	 * {@code n = wrapped.size()}.</p>
+	 * @param wrapped the nodes of a graph, wrapped in some type of WrapVertex
+	 * @param edgeDetector bidirectionally links two nodes from {@code wrapped} if and 
+	 * only if their raw nodes test true
+	 */
 	public static <T extends WrapVertex<W,T>, W> void addEdges(
 			Collection<T> wrapped, 
 			BiPredicate<? super W, ? super W> edgeDetector){
@@ -117,10 +133,32 @@ public class Wrap<W> implements WrapVertex<W,Wrap<W>>{
 		}
 	}
 	
-	public static void addNodes(){
-		
+	/**
+	 * <p>A utility method that extracts a graph's nodes from its {@code edges} and 
+	 * wraps those nodes in {@code Wrap}s.</p>
+	 * @param edges a {@link Collection} of edges of the graph of whose interconnected 
+	 * nodes a list is returned
+	 * @return a list of the nodes of the graph whose edges are specified 
+	 * in {@code edges}
+	 * @throws IllegalArgumentException if any of the {@code edges} has a size 
+	 * other than {@value #VERTICES_PER_EDGE}
+	 */
+	public static <E extends Collection<W>, W> List<Wrap<W>> wrap(Collection<E> edges){
+		return wrap(edges, Wrap<W>::new);
 	}
 	
+	/**
+	 * <p>A utility method that extracts a graph's nodes from its {@code edges} and 
+	 * wraps those nodes with {@code WrapVertex}s via {@code wrapper}.</p>
+	 * @param edges a {@link Collection} of edges of the graph of whose interconnected 
+	 * nodes a list is returned
+	 * @param wrapper specifies the type of {@code WrapVertex} that wraps raw node 
+	 * elements of edges from {@code edges}
+	 * @return a list of the nodes of the graph whose edges are specified 
+	 * in {@code edges}
+	 * @throws IllegalArgumentException if any of the specified {@code edges} has a 
+	 * size not equal to {@value #VERTICES_PER_EDGE}
+	 */
 	public static <T extends WrapVertex<W,T>, E extends Collection<W>, W> List<T> wrap(
 			Collection<E> edges, 
 			Function<? super W, T> wrapper){
@@ -131,6 +169,19 @@ public class Wrap<W> implements WrapVertex<W,Wrap<W>>{
 		return wrappedNodes;
 	}
 	
+	/**
+	 * <p>A utility method that adds elements of {@code wrappedNodes} as 
+	 * {@link Vertex#neighbors() neighbors} of each other such that all the edges 
+	 * specified by {@code edges} are accounted for.</p>
+	 * <p>This method operates in {@code O(n)} time where 
+	 * {@code n = edges.size()}.</p>
+	 * @param edges the edges of the graph whose nodes are contained in 
+	 * {@code wrappedNodes}
+	 * @param wrappedNodes the wrapped nodes of the graph whose edges are contained 
+	 * in {@code edges}
+	 * @throws IllegalArgumentException if any of the {@code edges} has a size 
+	 * not equal to {@value #VERTICES_PER_EDGE}
+	 */
 	public static <T extends WrapVertex<W,T>, E extends Collection<W>, W> void addNodes(
 			Collection<E> edges, 
 			List<T> wrappedNodes){
@@ -151,6 +202,23 @@ public class Wrap<W> implements WrapVertex<W,Wrap<W>>{
 				});
 	}
 	
+	/**
+	 * <p>A utility method that wraps the raw nodes of a graph whose edges are 
+	 * explicitly specified as {@code edges} and whose nodes are implicitly 
+	 * specified via the application of {@code nodeSource} to the elements of 
+	 * {@code edges}. The extracted raw nodes are wrapped via {@code wrapper}.</p>
+	 * @param edges the edges of the graph in a form that {@code nodeSource} can 
+	 * translate into a size-{@value #VERTICES_PER_EDGE} Collection of raw nodes
+	 * @param nodeSource translates a raw edge to a size-{@value #VERTICES_PER_EDGE} 
+	 * Collection of raw nodes
+	 * @param wrapper wraps the raw nodes that {@code nodeSource} extracts from 
+	 * the elements of {@code edges}
+	 * @return a List of wrapped, connected nodes of the graph whose {@code edges} 
+	 * were provided
+	 * @throws IllegalArgumentException if any of the Collections produced by 
+	 * an application of {@code nodeSource} has a size other than 
+	 * {@value #VERTICES_PER_EDGE}
+	 */
 	public static <T extends WrapVertex<W,T>, E, W> List<T> wrap(
 			Collection<E> edges, 
 			Function<? super E, ? extends Collection<W>> nodeSource, 
