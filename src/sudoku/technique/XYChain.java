c8fe1274c883b609ecbe8137f64f4da43193f8a2
fiveham <fiveham@users.noreply.github.com>
2016-06-19 14:24:01 -0400

    refactor XYChain, documentation

--- src/sudoku/technique/XYChain.java
+++ src/sudoku/technique/XYChain.java
@@ -86,22 +86,19 @@ public class XYChain extends AbstractTechnique {
 		
 		for(Graph<ColorClaim> falsePush : allFalsePushes.connectedComponents()){
 			
-			List<Pair<Predicate<ColorClaim>, Graph<ColorClaim>>> mixPushes = ColorClaim.SIGNS.stream()
-					.map((sign) -> new Pair<>(sign, pushGraph(falsePush, sign, falseMap, trueMap)))
-					.collect(Collectors.toList());
-
-			List<Fact> xorChainsInXYChain = target.factStream()
-					.filter(Fact::isXor)
+			List<Pair<Predicate<ColorClaim>, Pair<Graph<ColorClaim>, Map<Claim,ColorClaim>>>> mixPushes = ColorClaim.SIGNS.stream()
+					.map((sign) -> new Pair<>(sign, mixPushGraphAndMap(falsePush, sign, falseMap, trueMap)))
 					.collect(Collectors.toList());
 			
-			for(Fact xor : xorChainsInXYChain){
+			for(Set<Claim> xorChain : xorChainsInXYChain(falsePush, trueMap)){
+				Claim representative = xorChain.iterator().next();
 				
 				Set<Claim> and = mixPushes.stream()
-						.map((pair) -> consequences(pair.getA(), xor.iterator().next(), pair.getB()))
+						.map((pair) -> consequences(pair.getA(), representative, pair.getB().getA(), pair.getB().getB()))
 						.collect(Collector.of(
 								HashSet::new, 
 								Set::addAll, 
-								(HashSet<Claim> left, HashSet<Claim> right) -> {
+								(left, right) -> {
 									left.retainAll(right);
 									return left;
 								}));
@@ -133,27 +130,54 @@ public class XYChain extends AbstractTechnique {
 		return null;
 	}
 	
+	private static List<Set<Claim>> xorChainsInXYChain(Graph<ColorClaim> falsePush, Map<Claim,ColorClaim> trueMap){
+		return new BasicGraph<ColorClaim>(
+				falsePush.nodeStream()
+						.map(ColorClaim::wrapped)
+						.map(trueMap::get)
+						.collect(Collectors.toList()))
+				.connectedComponents().stream()
+						.map((graph) -> graph.nodeStream()
+								.map(ColorClaim::wrapped)
+								.collect(Collectors.toSet()))
+						.collect(Collectors.toList());
+	}
+	
+	/**
+	 * <p>Returns a set of the Claims falsified if {@code xorElement} takes 
+	 * on the state (true or false) that {@code state} outputs when given 
+	 * as input the {@code ColorClaim} in {@code mixPush} that wraps 
+	 * {@code xorElement}.</p>
+	 * @param state indicates whether {@code xorElement} is to be treated as 
+	 * true or false
+	 * @param xorElement a claim from a xor-chain subgraph of {@code mixPush}
+	 * @param mixPush a directed graph where one claim points to another claim 
+	 * if the first claim, by having a certain state, forces the other claim 
+	 * to have the opposite state. The forcing relationship only works for one 
+	 * of the two possible states of the first claim; if the first claim has 
+	 * the wrong state, it does not force the other claim's state.
+	 * @return
+	 */
 	private static Set<Claim> consequences(
 			Predicate<ColorClaim> state, 
 			Claim xorElement, 
-			Graph<ColorClaim> mixPush){
-		Set<Claim> falsified = new HashSet<>();
-		
-		ColorClaim seed = mixPush.nodeStream().filter((cc) -> cc.wrapped().equals(xorElement)).findFirst().get();
+			Graph<ColorClaim> mixPush,
+			Map<Claim,ColorClaim> mixPushMap){
+		ColorClaim seed = mixPushMap.get(xorElement);
 		
 		Graph<ColorClaim> component = mixPush.component(
 				mixPush.nodeStream().collect(Collectors.toList()), 
 				(list) -> seed, 
 				Collections.emptyList());
 		
-		component.nodeStream()
-				.filter(state)
-				.forEach((c) -> falsified.addAll(c.wrapped().visible()));
-		
-		return falsified;
+		return component.nodeStream()
+				.filter(state) //these ones falsify Claims visible() to them
+				.map(ColorClaim::wrapped)
+				.map(Claim::visible)
+				.collect(Sledgehammer.massUnionCollector());
 	}
 	
-	private static Graph<ColorClaim> pushGraph(
+	private static Pair<Graph<ColorClaim>, Map<Claim,ColorClaim>> mixPushGraphAndMap(
 			Graph<ColorClaim> falsePush, 
 			Predicate<ColorClaim> state, 
 			Map<Claim,ColorClaim> falseMap, 
@@ -178,7 +202,9 @@ public class XYChain extends AbstractTechnique {
 			cc.neighbors().addAll(neighbors);
 		}
 		
-		return new BasicGraph<>(pushGraphNodes);
+		return new Pair<>(
+				new BasicGraph<>(pushGraphNodes), 
+				Wrap.rawToWrap(pushGraphNodes));
 	}
 	
 	private static Map<Claim,ColorClaim> trueMap(Map<Claim,ColorClaim> falseMap, Collection<Fact> xorFacts){
