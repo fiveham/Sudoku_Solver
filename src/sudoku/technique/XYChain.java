359e73f6abfce6d684738b835e68aba798291033
fiveham <fiveham@users.noreply.github.com>
2016-10-20 02:23:02 -0400

    documentation; improve names

--- src/sudoku/technique/XYChain.java
+++ src/sudoku/technique/XYChain.java
@@ -9,8 +9,22 @@ import sudoku.Fact;
 import sudoku.Sudoku;
 import sudoku.time.TechniqueEvent;
 
+/**
+ * <p>This solution technique explores the implications of either of the 
+ * two Claims of a two-Claim Rule being true. Any Claims that would be 
+ * made false by either Claim being true must be false.</p>
+ * @author fiveham
+ * @see http://www.sadmansoftware.com/sudoku/xychain.php
+ *
+ */
 public class XYChain extends AbstractTechnique {
 	
+	/**
+	 * <p>Constructs an XYChain that works to solve the specified 
+	 * {@code puzzle}.</p>
+	 * @param puzzle the Puzzle that this solution technique 
+	 * instance works to solve
+	 */
 	public XYChain(Sudoku puzzle) {
 		super(puzzle);
 	}
@@ -33,71 +47,92 @@ public class XYChain extends AbstractTechnique {
 							Collector.Characteristics.IDENTITY_FINISH, Collector.Characteristics.UNORDERED));
 			
 			if(!falseIntersection.isEmpty()){
-				return new XYChainEvent(falseIntersection, binaryRule).falsifyClaims();
+				return new SolveEventXYChain(falseIntersection, binaryRule).falsifyClaims();
 			}
 		}
 		
 		return null;
 	}
 	
-	class XYChainEvent extends TechniqueEvent{
+	public class SolveEventXYChain extends TechniqueEvent{
 		
-		private final String binaryRuleString;
+		private final Fact binaryRule;
 		
-		public XYChainEvent(Set<Claim> falsified, Fact binaryRule) {
+		public SolveEventXYChain(Set<Claim> falsified, Fact binaryRule) {
 			super(falsified);
-			this.binaryRuleString = binaryRule.toString();
+			this.binaryRule = binaryRule;
 		}
 		
 		@Override
 		protected String toStringStart() {
-			return "XYChain: Either solution of "+binaryRuleString+" falsifies "+falsified();
+			return "XYChain: Either solution of "+binaryRule.toString()+" falsifies "+falsified();
 		}
-		
 	}
 	
+	/**
+	 * <p>Determines which Claims must be false if the specified 
+	 * Claim were true.</p>
+	 * <p>Determination that a Claim must be true is 
+	 * limited to the identification of Rules with a single non-false
+	 * Claim.  Other solution techniques' abilities to remotely detect 
+	 * the true state of Claims are not used.</p>
+	 * @param assertedTrue the Claim whose asserted truth is used to 
+	 * determine the states of other Claims in the puzzle
+	 * @return a Set of the Claims that must be false if {@code assertedTrue} 
+	 * were true.
+	 */
 	public static Set<Claim> getFalsifiedClaims(Claim assertedTrue){
 		
-		Set<Claim> ver = new HashSet<>();
-		Set<Claim> fal = new HashSet<>();
+		Set<Claim> trueClaims = new HashSet<>();
+		Set<Claim> falseClaims = new HashSet<>();
 		
-		Set<Claim> newVer = new HashSet<>();
-		newVer.add(assertedTrue);
+		Set<Claim> newTrue = new HashSet<>();
+		newTrue.add(assertedTrue);
 		
-		while(!newVer.isEmpty()){
-			Set<Claim> newFal = newFal(newVer, fal);
+		while(!newTrue.isEmpty()){
+			Set<Claim> newFalse = newFalse(newTrue, falseClaims);
 			
-			ver.addAll(newVer);
-			fal.addAll(newFal);
+			trueClaims.addAll(newTrue);
+			falseClaims.addAll(newFalse);
 			
-			newVer = newVer(fal, newFal);
+			newTrue = newTrue(falseClaims, newFalse);
 		}
 		
-		return fal;
+		return falseClaims;
 	}
 	
-	public static Set<Claim> newFal(Set<Claim> newVer, Set<Claim> fal){
+	/**
+	 * <p.Determines which Claims, in addition to those already known to 
+	 * be conditionally false, must be conditionally false.</p>
+	 * <p>A Claim is conditionally false if it would have to be false 
+	 * given that the Claim currently {@code assertedTrue} in 
+	 * {@code getFalsifiedClaims} is asserted to be true.</p>
+	 * @param newTrue
+	 * @param f
+	 * @return
+	 */
+	private static Set<Claim> newFalse(Set<Claim> newTrue, Set<Claim> f){
 		Set<Claim> result = new HashSet<>();
-		for(Claim newlyVerified : newVer){
+		for(Claim newlyVerified : newTrue){
 			result.addAll(newlyVerified.visible());
 		}
-		result.removeAll(fal);
+		result.removeAll(f);
 		return result;
 	}
 	
 	/**
 	 * <p>Returns a set of the Claims that must be true because all other Claims of some 
 	 * Rule have already been determined to be false.</p>
-	 * @param newFal
+	 * @param newFalse
 	 * @return
 	 */
-	public static Set<Claim> newVer(Set<Claim> fal, Set<Claim> newFal){
+	private static Set<Claim> newTrue(Set<Claim> f, Set<Claim> newFalse){
 		
-		Set<Fact> visibleRules = visibleRules(newFal);
+		Set<Fact> visibleRules = visibleRules(newFalse);
 		Set<Claim> result = new HashSet<>(visibleRules.size());
 		for(Fact rvisibleRule : visibleRules){
 			Set<Claim> copyOfVisibleRule = new HashSet<>(rvisibleRule);
-			copyOfVisibleRule.removeAll(fal);
+			copyOfVisibleRule.removeAll(f);
 			if(copyOfVisibleRule.size() == Fact.SIZE_WHEN_SOLVED){
 				result.add(rvisibleRule.iterator().next());
 			}
@@ -108,11 +143,11 @@ public class XYChain extends AbstractTechnique {
 	
 	/**
 	 * <p>Returns a set of the Facts visible (adjacent) to at least one of 
-	 * the Claims in <code>newFal</code>.</p>
-	 * @param newFal
+	 * the Claims in {@code newFalse}.</p>
+	 * @param newFalse
 	 * @return
 	 */
-	public static Set<Fact> visibleRules(Set<Claim> newFal){
-		return Sledgehammer.massUnion(newFal);
+	private static Set<Fact> visibleRules(Set<Claim> newFalse){
+		return Sledgehammer.massUnion(newFalse);
 	}
 }
