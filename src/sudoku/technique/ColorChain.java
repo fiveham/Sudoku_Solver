a2fe19d6efabd427727bab966bf762a5f81f4d95
fiveham <fiveham@users.noreply.github.com>
2016-11-07 17:09:20 -0500

    make implications() de facto the only subtechnique

--- src/sudoku/technique/ColorChain.java
+++ src/sudoku/technique/ColorChain.java
@@ -1,28 +1,14 @@
 package sudoku.technique;
 
 import common.BackedSet;
-import common.Pair;
 import common.Sets;
-import common.graph.Graph;
-import common.graph.Wrap;
-import common.graph.BasicGraph;
-import common.graph.WrapVertex;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.Set;
-import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import sudoku.Claim;
@@ -53,141 +39,9 @@ public class ColorChain extends AbstractTechnique {
 		super(puzzle);
 	}
 	
-	/**
-	 * <p>Isolates those Rules in the target that have two Claims, makes 
-	 * a Graph of their Claims, where two Claims share an edge if they 
-	 * share a Rule as an element, and returns a collection of that Graph's 
-	 * connected components, each of which is a xor-chain with two possible 
-	 * solution-states.</p>
-	 * 
-	 * @return a collection of the xor-chains that exist in the target 
-	 * at the time when this method is called
-	 */
-	private static Collection<Graph<ColorClaim>> generateChains(Sudoku target, ColorSource colorSource){
-		List<Fact> xorRules = target.factStream()
-				.filter(Fact::isXor)
-				.collect(Collectors.toList());
-		return new BasicGraph<ColorClaim>(Wrap.wrap(xorRules,ColorClaim::new))
-				.addGrowthListenerFactory(colorSource)
-				.connectedComponents();
-	}
-
-	private final ColorSource colorSource = new ColorSource();
-	
-	/**
-	 * <p>A generator and manager of ints to be used as colors for Claims 
-	 * in xor-chains.</p>
-	 * 
-	 * <p>Use {@code get()} to get the current color with the current sign, 
-	 * use {@code nextColor()} when beginning to color a new xor-chain, and 
-	 * use {@code invertColor()} to change the sign of the colors returned 
-	 * by subsequent calls to {@code get()}.</p>
-	 * @author fiveham
-	 *
-	 */
-	private static class ColorSource implements Supplier<Consumer<Set<ColorClaim>>>{
-		
-		public static final int INIT_COLOR = 1;
-		
-		private int color;
-		
-		ColorSource(){
-			this.color = INIT_COLOR;
-		}
-		
-		@Override
-		public Consumer<Set<ColorClaim>> get() {
-			return new Colorizer(color++);
-		}
-		
-		class Colorizer implements Consumer<Set<ColorClaim>>{
-			
-			private final int col;
-			private boolean positive;
-			
-			Colorizer(int col){
-				this.col = col;
-				this.positive = true;
-			}
-			
-			@Override
-			public void accept(Set<ColorClaim> cuttingEdge) {
-				cuttingEdge.stream().forEach((e)->e.setColor(getColor()));
-				invertColor();
-			}
-			
-			/**
-			 * <p>Changes this Colorizer's color sign so that 
-			 * subsequent calls to {@code getColor()} return a color 
-			 * with the sign opposite of the sign returned by 
-			 * previous calls to {@code getColor()}.</p>
-			 */
-			private void invertColor(){
-				positive = !positive;
-			}
-			
-			/**
-			 * <p>Returns the current color with the current sign.</p>
-			 * @return the current color with the current sign
-			 */
-			private int getColor(){
-				return positive ? col : -col;
-			}
-		}
-	}
-	
-	private static final List<Function<ColorChain,TechniqueEvent>> SUBTECHNIQUES = Arrays.asList(
-			ColorChain::subsumedFacts,
-			ColorChain::xyChain, 
-			ColorChain::implicationIntersection);
-	
 	@Override
 	public TechniqueEvent process(){
-		try{
-			return SUBTECHNIQUES.stream()
-					.map((st) -> st.apply(this))
-					.filter((result) -> result != null)
-					.findFirst().get();
-		} catch(NoSuchElementException e){
-			return null;
-		}
-	}
-	
-	private static final int MIN_IMPLICATION_INTERSECTION_SIZE = 3;
-	
-	/**
-	 * <p>Explores the consequences of each individual Claim of a 
-	 * given Rule (for each Rule of each size greater than 2) being 
-	 * true and falsifies those Claims that would have to be false 
-	 * regardless of which Claim of the Rule currently in focus is 
-	 * true.</p>
-	 * @return
-	 */
-	private TechniqueEvent implicationIntersection(){
-		for(int size = MIN_IMPLICATION_INTERSECTION_SIZE; size <= target.sideLength(); ++size){
-			
-			Collection<Fact> rules;
-			{
-				final int size2 = size;
-				rules = target.factStream()
-						.filter((f) -> f.size() == size2)
-						.collect(Collectors.toList());
-			}
-			
-			for(Fact rule : rules){
-				
-				Set<Claim> externalFalseIntersection = getFalseIntersection(rule, size);
-				
-				if(!externalFalseIntersection.isEmpty()){
-					return new SolveEventImplicationIntersection(
-							externalFalseIntersection, 
-							rule)
-							.falsifyClaims();
-				}
-			}
-		}
-		
-		return null;
+		return implications();
 	}
 	
 	public static class SolveEventImplicationIntersection extends TechniqueEvent{
@@ -205,131 +59,6 @@ public class ColorChain extends AbstractTechnique {
 		}
 	}
 	
-	private static Set<Claim> getFalseIntersection(Fact rule, int size){
-		
-		Iterator<Claim> claimAsserter = rule.iterator();
-		
-		if(!claimAsserter.hasNext()){
-			StringBuilder sb = new StringBuilder("This Iterator doesn't have a first element. ");
-			sb.append("Size: ").append(rule.size());
-			sb.append("Intended size: ").append(size);
-			throw new IllegalStateException(sb.toString());
-		}
-		
-		Set<Claim> falseIntersection = getFalsifiedClaims(claimAsserter.next());
-		
-		while(claimAsserter.hasNext() && !rule.containsAll(falseIntersection)){
-			falseIntersection.retainAll(
-					getFalsifiedClaims(
-							claimAsserter.next()));
-		}
-		
-		falseIntersection.removeAll(rule);
-		return falseIntersection;
-	}
-	
-	private TechniqueEvent xyChain(){
-		for(Graph<ColorClaim> chain : generateChains(target, colorSource)){ //for each chain
-			Set<Claim> falseIntersection = ColorClaim.COLOR_SIGNS.stream()
-					.map((test) -> chain.nodeStream()
-							.filter(test)
-							.map(ColorClaim::wrapped)
-							.collect(Collectors.toSet()))
-					.map(ColorChain::getFalsifiedClaims)
-					.collect(Sets.massIntersectionCollector()); //get the overlap of the implied falsifications of both solutions
-			
-			if(!falseIntersection.isEmpty()){
-				List<Fact> xorChain = chain.nodeStream()
-						.map(ColorClaim::wrapped)
-						.collect(Sets.massUnionCollector())
-						.stream()
-						.filter(Fact::isXor)
-						.collect(Collectors.toList());
-				return new SolveEventColorChain(falseIntersection, xorChain)
-						.falsifyClaims();
-			}
-		}
-		
-		return null;
-	}
-	
-	private static Set<Claim> getFalsifiedClaims(Claim... initialTrue){
-		return getFalsifiedClaims(
-				Arrays.stream(initialTrue)
-				.collect(Collectors.toSet()));
-	}
-	
-	private static Set<Claim> getFalsifiedClaims(Set<Claim> initialTrue){
-		
-		Set<Claim> trueClaims = new HashSet<>();
-		Set<Claim> falseClaims = new HashSet<>();
-		
-		Set<Claim> newTrue = new HashSet<>(initialTrue);
-		
-		while(!newTrue.isEmpty()){
-			Set<Claim> newFalse = newFalse(newTrue, falseClaims);
-			
-			trueClaims.addAll(newTrue);
-			falseClaims.addAll(newFalse);
-			
-			newTrue = newTrue(falseClaims, trueClaims, newFalse);
-		}
-		
-		return falseClaims;
-	}
-	
-	/**
-	 * <p.Determines which Claims, in addition to those already known to 
-	 * be conditionally false, must be conditionally false.</p>
-	 * <p>A Claim is conditionally false if it would have to be false 
-	 * given that the Claim currently {@code assertedTrue} in 
-	 * {@code getFalsifiedClaims} is asserted to be true.</p>
-	 * @param newTrue
-	 * @param f
-	 * @return
-	 */
-	private static Set<Claim> newFalse(Set<Claim> newTrue, Set<Claim> f){
-		Set<Claim> result = new HashSet<>();
-		for(Claim newlyVerified : newTrue){
-			result.addAll(newlyVerified.visible());
-		}
-		result.removeAll(f);
-		return result;
-	}
-	
-	/**
-	 * <p>Returns a set of the Claims that must be true because all other Claims of some 
-	 * Rule have already been determined to be false.</p>
-	 * @param newFalse
-	 * @return
-	 */
-	private static Set<Claim> newTrue(Set<Claim> falseClaims, Set<Claim> trueClaims, Set<Claim> newFalse){
-		
-		Set<Fact> visibleRules = visibleRules(newFalse);
-		Set<Claim> result = new HashSet<>(visibleRules.size());
-		for(Fact rvisibleRule : visibleRules){
-			Set<Claim> copyOfVisibleRule = new HashSet<>(rvisibleRule);
-			copyOfVisibleRule.removeAll(falseClaims);
-			if(copyOfVisibleRule.size() == Fact.SIZE_WHEN_SOLVED){
-				result.add(rvisibleRule.iterator().next());
-			}
-		}
-		
-		result.removeAll(trueClaims);
-		
-		return result;
-	}
-	
-	/**
-	 * <p>Returns a set of the Facts visible (adjacent) to at least one of 
-	 * the Claims in {@code newFalse}.</p>
-	 * @param newFalse
-	 * @return
-	 */
-	private static Set<Fact> visibleRules(Set<Claim> newFalse){
-		return Sets.massUnion(newFalse);
-	}
-	
 	public class SolveEventColorChain extends TechniqueEvent{
 		
 		private final Collection<Fact> xorEntity;
@@ -345,95 +74,9 @@ public class ColorChain extends AbstractTechnique {
 		}
 	}
 	
-	/**
-	 * <p>Wraps a Claim and decorates it with an int color.</p>
-	 * @author fiveham
-	 *
-	 */
-	private static class ColorClaim implements WrapVertex<Claim,ColorClaim>{
-		
-		public static final List<Predicate<ColorClaim>> COLOR_SIGNS = Arrays.asList(
-				(colorClaim) -> colorClaim.color > 0, 
-				(colorClaim) -> colorClaim.color < 0);
-		
-		private int color = 0;
-		private final Claim claim;
-		private final List<ColorClaim> neighbors;
-		
-		ColorClaim(Claim claim){
-			this.claim = claim;
-			this.neighbors = new ArrayList<>();
-		}
-		
-		@Override
-		public Claim wrapped(){
-			return claim;
-		}
-		
-		@Override
-		public List<ColorClaim> neighbors(){
-			return neighbors;
-		}
-		
-		/**
-		 * <p>Sets the color to {@code color{@code .</p>
-		 * @param color the new color
-		 */
-		void setColor(int color){
-			if(this.color == 0){
-				this.color = color;
-			} else{
-				throw new IllegalStateException("Cannot change color to "+color+" because color has already been set to "+this.color);				
-			}
-		}
-		
-		@Override
-		public boolean equals(Object o){
-			if(o instanceof ColorClaim){
-				ColorClaim cc = (ColorClaim) o;
-				return cc.color == this.color && cc.claim == this.claim;
-			}
-			return false;
-		}
-		
-		@Override
-		public int hashCode(){
-			return claim.hashCode();
-		}
-		
-		@Override
-		public String toString(){
-			return "ColorClaim pairing " + color + " with " + claim;
-		}
-	}
-	
 	@Override
 	public ColorChain apply(Sudoku sudoku){
 		return new ColorChain(sudoku);
-	}
-	
-	/**
-	 * <p>Finds a unary Fact in {@code target} and sets that Fact's 
-	 * one Claim neighbor true.</p>
-	 * @return an Initialization describing the verification of 
-	 * an Init's sole Claim neighbor and any and all resulting 
-	 * automatic resolution events, or null if no Init is found
-	 */
-	private TechniqueEvent subsumedFacts(){
-		Optional<Pair<Fact,Set<Fact>>> i = target.factStream()
-				.map((fact) -> new Pair<Fact,Set<Fact>>(
-						fact, 
-						fact.visible().stream()
-								.filter((vis) -> vis.containsAll(fact))
-								.collect(Collectors.toSet())))
-				.filter((pair) -> !pair.getB().isEmpty())
-				.findFirst();
-		if(i.isPresent()){
-			Pair<Fact,Set<Fact>> p = i.get();
-			return new SubsumedFact(p.getA(), p.getB()).falsifyClaims();
-		}
-		
-		return null;
 	}
 	
 	/**
@@ -482,7 +125,6 @@ public class ColorChain extends AbstractTechnique {
 	 * {@code null} if no progress was made
 	 */
 	private TechniqueEvent implications(){
-		
 		for(Fact f : target.factStream()
 				.sorted(SMALL_TO_LARGE)
 				.collect(Collectors.toList())){
