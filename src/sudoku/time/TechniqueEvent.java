9857e182515d74c8128db1276363afd20b1d09a4
fiveham <fiveham@users.noreply.github.com>
2017-01-18 06:29:18 -0500

    update javadoc

--- src/sudoku/time/TechniqueEvent.java
+++ src/sudoku/time/TechniqueEvent.java
@@ -4,17 +4,24 @@ import sudoku.Claim;
 import java.util.Set;
 
 /**
- * <p>A time node denoting and encapsulating an event in which some progress is made in solving the
- * target.</p>
+ * <p>Denotes an event in which progress is made in solving a sudoku puzzle.</p>
  * @author fiveham
- * @author fiveham
- *
  */
 public abstract class TechniqueEvent extends FalsifiedTime{
+  
+  /**
+   * <p>Constructs a TechniqueEvent for a solving event in which the claims in {@code falsified} 
+   * were determined to be false.</p>
+   * @param falsified claims determined to be false in the event described by this object
+   */
 	public TechniqueEvent(Set<Claim> falsified){
 		super(null, falsified);
 	}
 	
+	/**
+	 * <p>Sets false the Claims that this event {@link #falsified() determined were false}.</p>
+	 * @return this TechniqueEvent
+	 */
 	@Override
 	public final TechniqueEvent falsifyClaims(){
 		super.falsifyClaims();
