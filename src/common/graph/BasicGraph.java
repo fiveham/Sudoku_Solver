3b743b90864dee0db634a6d18c490837c18f5f13
fiveham <fiveham@users.noreply.github.com>
2017-01-18 04:37:29 -0500

    improve documentation

--- src/common/graph/BasicGraph.java
+++ src/common/graph/BasicGraph.java
@@ -9,35 +9,31 @@ import java.util.function.Supplier;
 /**
  * <p>A graph.</p>
  * @author fiveham
- * @author fiveham
- *
  * @param <T> the type of the vertices of this Graph
- * @param <T> the type of the vertices of this Graph@param <T> the type of the vertices of this
- * @param <T> the type of the vertices of this GraphGraph
  */
 public class BasicGraph<T extends Vertex<T>> extends AbstractGraph<T>{
 	
-    /**
-     * <p>Constructs a BasicGraph with no vertices and no contraction-event-listener sources.</p>
-     */
+  /**
+   * <p>Constructs a BasicGraph with no vertices.</p>
+   */
 	public BasicGraph() {
 		super();
 	}
 	
-    /**
-     * <p>Constructs a BasicGraph having the vertices contained in {@code coll}.</p>
-     * @param coll vertices for the graph being constructed
-     */
+  /**
+   * <p>Constructs a BasicGraph having the vertices contained in {@code coll}.</p>
+   * @param coll vertices for the graph
+   */
 	public BasicGraph(Collection<? extends T> coll){
 		super(coll);
 	}
 	
-    /**
-     * <p>Constructs a BasicGraph having the specified vertices and contraction-event-listener
-     * sources</p>
-     * @param coll
-     * @param factories
-     */
+  /**
+   * <p>Constructs a BasicGraph having the specified vertices and growth-event-listener
+   * sources</p>
+   * @param coll
+   * @param factories
+   */
 	public BasicGraph(Collection<? extends T> coll, List<Supplier<Consumer<Set<T>>>> factories) {
 		super(coll, factories);
 	}
