e9d549513de6acfd113d78bdfaf470421a3a5e56
fiveham <fiveham@users.noreply.github.com>
2017-01-19 08:26:44 -0500

    update javadoc in Parser

--- src/sudoku/parse/Parser.java
+++ src/sudoku/parse/Parser.java
@@ -3,30 +3,37 @@ package sudoku.parse;
 import java.util.List;
 
 /**
- * <p>Parses a file into a sudoku puzzle.</p> <p>A Parser should accept a File or another reference
- * to a file's contents, such as a Scanner or a String, and determine the magnitude of the specified
- * puzzle and the initial contents of that puzzle.</p> <p>The file must be parsed before any calls
- * to any of this object's puzzle-pertinent instance methods return--preferably before the
- * constructor returns.</p>
+ * <p>Parses a sudoku puzzle out of a file, identifying the initial {@link #values() values} held by 
+ * the cells of the puzzle.</p>
+ * <p>A Parser should accept a File or another reference
+ * to a file's contents, such as a Scanner or a String, and determine the magnitude and initial 
+ * cell values of the puzzle.</p>
+ * <p>The file must be parsed before any calls to any of this object's puzzle-pertinent instance 
+ * methods return--preferably before the constructor returns.</p>
  * @author fiveham
- * @author fiveham
- *
  */
 public interface Parser {
 	
+  /**
+   * <p>The largest allowed numerical base for human-readable representations of a Puzzle.</p>
+   * <p>Equal to 36 because that is the number of digits 0-9 plus the number of letters in the 
+   * English alphabet. Coincidentally, this is also the appropriate base for printing a puzzle with 
+   * a magnitude of 6.</p>
+   */
 	public static final int MAX_RADIX = 36;
 	
-    /**
-     * <p>The magnitude of the puzzle parsed from the file. A Puzzle's
-     * {@link sudoku.Sudoku#magnitude() magnitude} is the square root of that puzzle's
-     * side-length.</p>
-     * @return the magnitude of the puzzle parsed from the file
-     */
+  /**
+   * <p>The {@link Sudoku#magnitude() magnitude} of the puzzle parsed from the file, equal to the 
+   * square root of the puzzle's side-length.</p>
+   * @return the magnitude of the puzzle parsed from the file
+   */
 	public int mag();
 	
-    /**
-     * <p>Returns a list of the values of the cells parsed from this Parser's file.</p>
-     * @return
-     */
+  /**
+   * <p>Returns a list of the values of the cells parsed from the file parsed by this Parser. The 
+   * values are listed in encounter order.</p>
+   * @return a list of the values of the cells parsed from the file parsed by this Parser in 
+   * encounter order
+   */
 	public List<Integer> values();
 }
