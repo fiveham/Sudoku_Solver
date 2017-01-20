eb83cdfd070897f2524077326caed4d08e701d8b
fiveham <fiveham@users.noreply.github.com>
2017-01-20 10:01:29 -0500

    remove unused methods, reduce redundancy

--- src/sudoku/Fact.java
+++ src/sudoku/Fact.java
@@ -12,17 +12,7 @@ public abstract class Fact extends NodeSet<Claim,Fact>{
      * <p>The number ({@value}) of elements (neighbors) of a Rule when the Claim satisfying the Rule
      * has been completely identified.</p>
      */
-	public static final int SIZE_WHEN_SOLVED = 1;
-	
-	public static final int TRUE_CLAIM_COUNT = SIZE_WHEN_SOLVED;
-	
-    /**
-     * <p>The number ({@value}) of elements (neighbors) of a Rule that is a {@code xor}. A Rule is a
-     * {@code xor} if it has two Claims, because exactly one of them is true and the other is false.
-     * Given that the Rule is satisfied (the Rule is {@code true}), the Claims, as inputs to the
-     * Rule, make such a Rule a {@code xor} operation on its neighbors.</p>
-     */
-	public static final int SIZE_WHEN_XOR = 2;
+	public static final int TRUE_CLAIM_COUNT = 1;
 	
 	protected Fact(Puzzle puzzle, Collection<Claim> c, int hash) {
 		super(puzzle, hash);
@@ -38,15 +28,7 @@ public abstract class Fact extends NodeSet<Claim,Fact>{
 		return false;
 	}
 	
-	public boolean insideVisible(){
-		return visible().stream().anyMatch((v) -> v.containsAll(this));
-	}
-	
 	public boolean isSolved(){
-		return size() == SIZE_WHEN_SOLVED;
-	}
-	
-	public boolean isXor(){
-		return size() == SIZE_WHEN_XOR;
+		return size() == TRUE_CLAIM_COUNT;
 	}
 }
