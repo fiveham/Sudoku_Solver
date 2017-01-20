c25b52b9f1ca69f89a1ddc31e8f18bf6145487c3
fiveham <fiveham@users.noreply.github.com>
2017-01-20 14:54:34 -0500

    remove unused methods, privatize everything possible, rename symbol to z

--- src/sudoku/Claim.java
+++ src/sudoku/Claim.java
@@ -15,52 +15,31 @@ public class Claim extends NodeSet<Fact,Claim>{
      */
 	private static final long serialVersionUID = -2402719833037606449L;
 	
-    /**
-     * <p>The initial number of {@link #neighbors() Rules to which this Claim belongs}. It is the
-     * count of the kinds of FactBags that can hold the same Claim in common: a Rule for a cell, a
-     * box, a column, and a row.</p>
-     */
-	public static final int INIT_OWNER_COUNT = Puzzle.RuleType.values().length;
+  /**
+   * <p>The initial number of {@link #neighbors() Rules to which this Claim belongs}. It is the
+   * number of {@link Puzzle.RuleType types} of Rules that can hold the same Claim in common: a 
+   * cell, a box, a column, and a row.</p>
+   */
+	private static final int INIT_OWNER_COUNT = Puzzle.RuleType.values().length;
 	
 	private final IndexValue x;
 	private final IndexValue y;
-	private final IndexValue symbol;
+	private final IndexValue z;
 	
-    /**
-     * <p>Constructs a Claim at the specified {@code x}, {@code y}, and {@code symbol} coordinates,
-     * pertaining to the specified {@code target}.</p>
-     * @param target the {@link Puzzle Puzzle} of whose underlying {@link common.graph.Graph Graph}
-     * this Claim is a {@link common.graph.Vertex vertex}
-     * @param x the x-coordinate of this Claim in claim-space
-     * @param y the y-coordinate of this Claim in claim-space
-     * @param symbol the z-coordinate of this Claim in claim-space
-     */
-	public Claim(Puzzle puzzle, IndexValue x, IndexValue y, IndexValue symbol) {
-		super(puzzle, INIT_OWNER_COUNT, linearizeCoords(x.intValue(), y.intValue(), symbol.intValue(), puzzle.sideLength()));
+  /**
+   * <p>Constructs a Claim at the specified {@code x}, {@code y}, and {@code z} coordinates, as a 
+   * part of the specified {@code puzzle}.</p>
+   * @param target the {@link Puzzle Puzzle} of whose underlying {@link common.graph.Graph Graph}
+   * this Claim is a {@link common.graph.Vertex vertex}
+   * @param x the x-coordinate of this Claim in claim-space
+   * @param y the y-coordinate of this Claim in claim-space
+   * @param z the z-coordinate of this Claim in claim-space
+   */
+	public Claim(Puzzle puzzle, IndexValue x, IndexValue y, IndexValue z) {
+		super(puzzle, INIT_OWNER_COUNT, linearizeCoords(x.intValue(), y.intValue(), z.intValue(), puzzle.sideLength()));
 		this.x = x;
 		this.y = y;
-		this.symbol = symbol;
-	}
-	
-    /**
-     * <p>Returns true if this Claim has been set false and all its internal properties correspond
-     * to that fact, false otherwise. No guarantee is made that formerly connected Facts reflect
-     * this Claim's falsehood at the time this method is called; they may still contain a reference
-     * to this Claim as a {@link #neighbors() neighbor}.</p>
-     * @return true if this Claim is known to be false, false otherwise
-     */
-	public boolean isSetFalse(){
-		return isEmpty();
-	}
-	
-    /**
-     * <p>Returns true if this Claim has been set true and all its internal properties correspond to
-     * that fact, false otherwise.</p>
-     * @return true if this Claim has been set true and all its internal properties correspond to
-     * that fact, false otherwise
-     */
-	public boolean isSetTrue(){
-		return stream().allMatch(Fact::isSolved);
+		this.z = z;
 	}
 	
     /**
@@ -83,20 +62,20 @@ public class Claim extends NodeSet<Fact,Claim>{
 		return y;
 	}
 	
-	public IndexValue getSymbol(){
-		return symbol;
+	public IndexValue getZ(){
+		return z;
 	}
 	
 	@Override
 	public String toString(){
-		return "Claim: cell "+x.humanReadableIntValue()+","+y.humanReadableIntValue()+" is "+symbol.humanReadableIntValue();
+		return "Claim: cell "+x.humanReadableIntValue()+","+y.humanReadableIntValue()+" is "+z.humanReadableIntValue();
 	}
 	
 	@Override
 	public boolean equals(Object o){
 		if( o instanceof Claim){
 			Claim c = (Claim) o;
-			return c.x == x && c.y == y && c.symbol == symbol && c.puzzle == puzzle;
+			return c.x == x && c.y == y && c.z == z && c.puzzle == puzzle;
 		}
 		return false;
 	}
@@ -109,20 +88,7 @@ public class Claim extends NodeSet<Fact,Claim>{
 	public double spaceDist(Claim otherClaim){
 		int dx = this.x.intValue() - otherClaim.x.intValue();
 		int dy = this.y.intValue() - otherClaim.y.intValue();
-		int dz = this.symbol.intValue() - otherClaim.symbol.intValue();
+		int dz = this.z.intValue() - otherClaim.z.intValue();
 		return Math.sqrt( dx*dx + dy*dy + dz*dz );
-	}
-	
-    /**
-     * <p>Returns a vector in claim-space from this Claim to {@code cn}.</p>
-     * @param cn another Claim
-     * @return a vector in claim-space from this Claim to {@code cn}
-     */
-	public int[] vectorTo(Claim cn) {
-		int[] result = new int[Puzzle.DIMENSION_COUNT];
-		result[Puzzle.X_DIM] = cn.x.intValue() - x.intValue();
-		result[Puzzle.Y_DIM] = cn.y.intValue() - y.intValue();
-		result[Puzzle.Z_DIM] = cn.symbol.intValue() - symbol.intValue();
-		return result;
 	}
 }
